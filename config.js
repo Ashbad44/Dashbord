d7cb120b30d6934cf91d4640a914835b99c3f100
Ashbad44 <badralmutairi3@gmail.com>
2026-08-04 22:32:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@
 const CONFIG = {
   // Paste the "Web app" URL you get after deploying Code.gs
   // Looks like: https://script.google.com/macros/s/AKfycb.../exec
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzYhMG8LQd0WzM0OwO9-_6F8dyE3xWdBgve64L1BUatOZ3TKNosuiuIQgfVFTdwZ3S0AQ/exec",
 
   // The same secret key you set inside Code.gs (SHEET_KEY)
-  ACCESS_KEY: "",
+  ACCESS_KEY: "Badr04071993",
 
   // How often to auto-refresh the page data, in milliseconds
   // (5 * 60 * 1000 = every 5 minutes). Set to 0 to disable auto-refresh.
